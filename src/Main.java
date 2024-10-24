000c750dca627a3ffa70004a31159079295bc766
VictorSundberg1 <victor.sundberg1@hotmail.com>
2024-10-24 09:16:20 +0200

    Updated the row and column markers to be on the right and under. also made the rows letters instead of number

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-//        Menu menu = new Menu();
-//        menu.displayMenu();
-        GameBoard board = new GameBoard(11,11,10);
-        board.showBoard();
-        board.showFullBoard();
+        Menu menu = new Menu();
+        menu.displayMenu();
     }
 }
