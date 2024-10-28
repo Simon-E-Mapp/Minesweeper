cecd74c0f9bdcf61702fd400e5fd45a7ad9f4265
ElinFoUni <3ggapp24.tvinel@folkuniversitetet.nu>
2024-10-28 12:54:37 +0100

    Added InputHandler class

--- src/Menu.java
+++ src/Menu.java
@@ -136,24 +136,5 @@ public class Menu {
         } while (menuOpen);
     }
 
-    // Handles Integers
-    private int getPlayerInt() {
-        while (!sc.hasNextInt()) {
-            System.out.println("Please enter a number between 1-4");
-            sc.nextLine();
-        }
-        int playerInt = sc.nextInt();
-        sc.nextLine();
-        return playerInt;
-    }
-
-    //Handles different strings
-    //TODO add more functionality beyond "getplayername"
-    private String getPlayerName() {
-        String playerName = sc.nextLine();
-        System.out.println("Hi, " + playerName + "!");
-        return playerName;
-    }
-
 
 }
